7fabb7335912921befaedd1c810b210b4044a720
cwon.lee <wonmyth@naver.com>
2024-07-03 11:18:00 +0900

    [feature] page 44

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -5,8 +5,17 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number >  1)
-			result.push_back(number);
+		if (number > 1) {
+			if(number == 4)
+			{ 
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else
+			{
+				result.push_back(number);
+			}
+		}
 		return result;
 	}
 };
