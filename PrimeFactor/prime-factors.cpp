27a8847b7fd10b37396c7cee8366f91a2f18805c
cwon.lee <wonmyth@naver.com>
2024-07-03 11:47:36 +0900

    [refactoring] page 59

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -6,9 +6,8 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
-			int divisor = 2;
 			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisor = 2; number > 1; divisor++) {
+				for (int divisor = 2; number > 1; divisor++) {
 					while (number % divisor == 0) {
 						result.push_back(divisor);
 						number /= divisor;
