ffe6e2de49aa22a388020c7a393a930420c48af4
cwon.lee <wonmyth@naver.com>
2024-07-03 11:21:04 +0900

    [refactoring] page 45

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -6,13 +6,17 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
-			if(number == 4)
-			{ 
-				result.push_back(2);
-				result.push_back(2);
+			if(number == 4) {
+				if (number % 2 == 0) {				
+					result.push_back(2);
+					number /= 2;
+				}
+				if (number % 2 == 0) {				
+					result.push_back(2);
+					number /= 2;
+				}
 			}
-			else
-			{
+			else {			
 				result.push_back(number);
 			}
 		}
