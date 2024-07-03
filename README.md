ae38a858b7510134e5c438ca085c73a0d22033ec
cwon.lee <wonmyth@naver.com>
2024-07-03 10:30:40 +0900

    init PrimeFactor

---
+++ README.md
@@ -0,0 +1,3 @@
+## PrimeFactor
+
+프라임팩터 카타
---
+++ prime_factors-test/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../PrimeFactor/prime-factors.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
