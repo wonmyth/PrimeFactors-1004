be62004210790f3a2bd5eee1b61aa1250f86e4bb
cwon.lee <wonmyth@naver.com>
2024-07-03 11:09:12 +0900

    [refactoring] page 39

--- prime_factors-test/test.cpp
+++ prime_factors-test/test.cpp
@@ -4,13 +4,16 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
-	PrimeFactor pf;
-	vector<int> expected = {};
-	EXPECT_EQ(expected, pf.of(1));
+class PrimeFixture : public testing::Test {
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
-TEST(PrimeFactors, Of2) {
-	PrimeFactor pf;
-	vector<int> expected = {2};
-	EXPECT_EQ(expected, pf.of(2));
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
