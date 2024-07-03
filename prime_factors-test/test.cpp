cc15074869dd38315542e869330a8232d0e6678d
cwon.lee <wonmyth@naver.com>
2024-07-03 11:00:01 +0900

    [feature] page 34

--- prime_factors-test/test.cpp
+++ prime_factors-test/test.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 #include "../PrimeFactor/prime-factors.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <vector>
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor pf;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, pf.of(1));
+
 }
